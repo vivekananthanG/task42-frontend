e88aa18d1e8f40e158f26453370de9453f6202cc
Vivekananthan <vivekananthan191@gmail.com>
2022-02-04 16:54:30 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form action="">
+      <h1>Create Mentor</h1>
+      <label for="mid">Mentor id:</label>
+  <input type="text" id="mid" name="mid" placeholder='Mentor id' />
+  <label for="name">Mentor name:</label>
+  <input type="text" id="name" name="name" placeholder='Mentor name' />
+  <label for="skill">Mentor Skill:</label>
+  <input type="text" id="skill" name="skill" placeholder='Mentor skill' />
+  <button type="submit" value="Submit" >Submit</button>
+</form>
+<form action="">
+      <h1>Create Student</h1>
+      <label for="sid"> Student id:</label>
+  <input type="text" id="sid" name="sid" placeholder='Student id' />
+  <label for="sname"> Student name:</label>
+  <input type="text" id="sname" name="sname" placeholder='Student name' />
+  <label for="email">Student Email:</label>
+  <input type="text" id="email" name="email" placeholder='Student email' />
+  <button type="submit" value="Submit">Submit</button>
+</form>
+<div><h1>Show Students for a mentor</h1>
+
+</div>
     </div>
   );
 }
